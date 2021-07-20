c8d1f8fd82848157f0d52c28b9a66025cc72cbaf
Lucas Raphael <lucasraphael.ti@gmail.com>
2021-07-20 15:05:22 +0200

    feat: Add ui-card component

---
+++ src/app/shared/components/ui-card/ui-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-card',
+  templateUrl: './ui-card.component.html',
+  styleUrls: ['./ui-card.component.scss']
+})
+export class UiCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UiRippleDirective } from './directives/ui-ripple.directive';
 import { UiButtonComponent } from './components/ui-button/ui-button.component';
+import { UiCardComponent } from './components/ui-card/ui-card.component';
 
 @NgModule({
   declarations: [
     UiRippleDirective,
-    UiButtonComponent
+    UiButtonComponent,
+    UiCardComponent
   ],
   imports: [
     CommonModule
@@ -15,7 +17,8 @@ import { UiButtonComponent } from './components/ui-button/ui-button.component';
   exports: [
     CommonModule,
     UiRippleDirective,
-    UiButtonComponent
+    UiButtonComponent,
+    UiCardComponent
   ]
 })
 export class SharedModule { }
